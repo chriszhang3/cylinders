e132696f4ecc9e3b59323536fceab922cb139974
Christopher Zhang <christophertzhang@gmail.com>
2023-01-25 17:05:19 -0500

    small changes in executables

--- main.py
+++ main.py
@@ -8,20 +8,27 @@ def find_valid_partitions(H, num_cylinders, num_classes):
     for cd in C.get_iterator(H, num_cylinders):
         part = list_partitions(num_cylinders, num_classes)
         part = check_pants_relations(cd, part)
-        part = check_twist_rel(Twist(cd), num_classes, part)
+        part = check_twist_rel(Twist(cd), 3, part)
         part = check_homologous_cylinders(cd, part)
         output[cd] = part
     return output
-    
+
+def dict_extend(dict1, dict2):
+    for k, v in dict2.items():
+        if k in dict1:
+            dict1[k].extend(v)
+        else:
+            dict1[k] = v
 
 def main():
     C = CylinderDiagrams()
-    strata = []
-    strata.append(AbelianStratum(3, 1).components()[0])
-    # strata.append(AbelianStratum(2, 2).components()[1])
-    for H in strata:
-        valid = find_valid_partitions(H, 4, 3)
-        for k, v in valid.items():
+    H = AbelianStratum(2, 1, 1).components()[0]
+
+    valid = {}
+    for i in range(2, 5):
+        dict_extend(valid, find_valid_partitions(H, 5, i))
+    for k, v in valid.items():
+        if v:
             print(k)
             print(v)
 
