9bf353d7f3ab4f6df025dc02867e9f954996be65
Christopher Zhang <christophertzhang@gmail.com>
2023-02-15 15:11:42 -0500

    files

--- list.py
+++ list.py
@@ -3,11 +3,11 @@ from surface_dynamics.databases.flat_surfaces import CylinderDiagrams
 
 def main():
     C = CylinderDiagrams()
-    # H = AbelianStratum(3, 1).components()[0]
+    H = AbelianStratum(3, 1).components()[0]
     # H = AbelianStratum(2, 2).components()[1]
-    H = AbelianStratum(2, 1, 1).components()[0]
-    for cd in C.get_iterator(H, 4):
-        print(cd)
+    # H = AbelianStratum(2, 1, 1).components()[0]
+    for i, cd in enumerate(C.get_iterator(H, 4)):
+        print(f"{i+1}. {cd}")
 
 if __name__ == '__main__':
     main()
