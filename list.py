119b9eda560ce14af9a69823c313cfcd2c22109f
Christopher Zhang <christophertzhang@gmail.com>
2023-06-12 13:58:52 -0400

    starting tutorial

--- list.py
+++ list.py
@@ -10,7 +10,8 @@ def main():
     C = CylinderDiagrams()
     # H = AbelianStratum(3, 1).components()[0]
     # H = AbelianStratum(2, 2).components()[1]
-    H = AbelianStratum(2, 1, 1).components()[0]
+    # H = AbelianStratum(2, 1, 1).components()[0]
+    H = AbelianStratum(1, 1, 1, 1).components()[0]
     for i, cd in enumerate(C.get_iterator(H, 5)):
         tw = Twist(cd)
         # print(f"{i+1}. {cd} {tw.twist_dimension()}")
